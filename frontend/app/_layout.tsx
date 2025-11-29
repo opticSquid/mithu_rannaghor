a5c1d8ec2b4e2f872fba2a22ee0db7fea0ea7959
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-11-29 09:22:00 +0530

    fix(app): deleting app

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,7 +3,7 @@ import '@/global.css';
 import { NAV_THEME } from '@/lib/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
 
@@ -18,7 +18,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
+      <Tabs screenOptions={{ headerShown: false }} />
       <PortalHost />
     </ThemeProvider>
   );
